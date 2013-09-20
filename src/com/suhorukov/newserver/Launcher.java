5d7a25fe6baa55b255afceb1005f34c1bf4f0bd4
tantal <ib.medvedev@rambler.ru>
2013-09-20 19:15:17 +0700

    19-09

--- src/com/suhorukov/newserver/Launcher.java
+++ src/com/suhorukov/newserver/Launcher.java
@@ -8,12 +8,9 @@ import java.io.File;
 public class Launcher {
     public static void main(String[] args){
         File test = new File("D:\\");
-//        HTMLGenerator gen = new HTMLGenerator(test);
-//        System.out.println(gen.generateHTMLList());
         Server server = new Server(80, test);
         while(true){
             server.listen();
         }
-        //System.out.println(new MimetypesFileTypeMap().getContentType(test));
     }
 }
